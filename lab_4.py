ce830115539b139b6cd20b3bb6de35422735ff58
Karthik Kanagond <karthikkanagond5@gmail.com>
2026-09-18 11:30:58 +0530

    Fix typo in input function calls

--- lab_4.py
+++ lab_4.py
@@ -1,5 +1,5 @@
-a = float(intput("Enter first number:"))
-b = float(intput("Enter second number:"))
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
 
 print("Addition: ",a+b)
 print("Subtraction: ",a-b)
